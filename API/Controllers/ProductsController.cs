ba2e8b49b286ebb282d8506b136b1afd9364f23f
Jan Kowalski <Niuniek7655@interia.pl>
2024-01-20 14:03:27 +0100

    End of section 2

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext context;
+    public ProductsController(StoreContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<List<Product>> GetProducts()
+    {
+        var products = context.Products.ToList();
+
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<List<Product>> GetProduct(string id)
+    {
+        var product = context.Products.Find(id);
+
+        return Ok(product);
+    }
+}
